226f9fe2f8ea7494ba6624a7881fb3bb17548512
Peter <peter.olayinka@britecore.com>
2019-05-20 12:26:58 +0100

    added necessary header to csv

--- conversion.py
+++ conversion.py
@@ -3,7 +3,7 @@ import os
 import requests
 
 
-token = os.getenv('token')
+token = os.environ.get('TOKEN')
 
 # read all story ID
 with open('Clubhouse Backlog - clubhouseBacklog.csv') as story_file:
@@ -11,11 +11,54 @@ with open('Clubhouse Backlog - clubhouseBacklog.csv') as story_file:
     story_ids = [x[1] for x in csv_reader][1:]
 
 
-for _id in story_ids:
-    url = f'https://api.clubhouse.io/api/v2/stories/{_id}?token={token}'
-    resp = requests.get(url)
-    import pdb; pdb.set_trace()
-    pass
+with open('clubhouse conversion.csv', mode='w') as csv_file:
+    fieldnames = [
+        'entity_type',
+        'archived',
+        'created_at',
+        'updated_at',
+        'id',
+        'external_id',
+        'name',
+        'story_type',
+        'position',
+        'workflow_state_id',
+        'moved_at',
+        'started',
+        'started_at',
+        'started_at_override',
+        'completed',
+        'completed_at',
+        'completed_at_override',
+        'blocker',
+        'blocked',
+        'estimate',
+        'deadline',
+        'project_id',
+        'labels',
+        'requested_by_id',
+        'owner_ids',
+        'follower_ids',
+        'mention_ids',
+        'epic_id',
+        'story_links',
+        'app_url',
+        'description',
+        'branches',
+        'comments',
+        'commits',
+        'files',
+        'linked_files',
+        'tasks'
+    ]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+
+    for _id in story_ids:
+        url = f'https://api.clubhouse.io/api/v2/stories/{_id}?token={token}'
+        resp = requests.get(url)
+        writer.writerow(resp.json())
 
 
 
